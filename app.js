81c9a3aca35df1d243a3a80ce96a8126628c21a1
Fernando <fmti91@hotmail.com>
2018-10-24 19:34:11 -0300

    access control with CORS (Cross-Origin Resource Sharing)

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan'); //log
+const bodyParser = require('body-parser');
 
 //routes which should handle requests
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((res, req, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+}); 
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
